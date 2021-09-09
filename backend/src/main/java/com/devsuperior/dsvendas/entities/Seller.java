0fadd0ed3e4c8bc2f056d062501bf146e58669c5
Leirsom Cantarini <leirsomm@gmail.com>
2021-09-08 21:11:57 -0300

    Layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -7,8 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -17,7 +15,7 @@ import javax.persistence.Table;
 public class Seller {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private String name;
 	
